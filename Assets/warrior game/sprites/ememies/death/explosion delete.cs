db8300be2ddc9a2db510d98780efae4ac264b479
AlfieMortimer <AlfieMortimer@users.noreply.github.com>
2023-10-03 16:04:55 +0100

    Update explosion delete.cs

--- Assets/warrior game/sprites/ememies/death/explosion delete.cs
+++ Assets/warrior game/sprites/ememies/death/explosion delete.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class explosiondelete : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(animator.GetCurrentAnimatorClipInfo)
     }
 }
